fa0107c0391bce3a7aeaf061cbb813e0bd79643d
issaiya1910 <issaiya.baigereyev@gymshark.com>
2023-07-11 01:10:42 +0600

    branch statistic

--- db/queries/branch.sql
+++ db/queries/branch.sql
@@ -10,7 +10,7 @@ VALUES ($1, $2, $3);
 -- name: GetBranchGoalByGivenDateRange :one
 SELECT COALESCE(bg.amount, 0) AS goal_amount
 FROM branch_goals_by_types bg
-WHERE bg.id = $1
+WHERE bg.branch_id = $1
   AND bg.from_date = $2
   AND bg.to_date = $3
   AND bg.type_id = $4;
