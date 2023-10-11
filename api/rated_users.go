60a03c605b1fba718287682c3a4b64b3dbbe1064
isaya1910 <isaya1910@gmail.com>
2023-10-12 00:39:55 +0600

    rating users

--- api/rated_users.go
+++ api/rated_users.go
@@ -12,8 +12,8 @@ type GetOrderedUsersRequest struct {
 	BrandId  int32  `json:"brand_id" form:"brand_id" binding:"required"`
 	Month    int32  `json:"month" form:"month" binding:"required"`
 	Year     int32  `json:"year" form:"year" binding:"required"`
-	Page     int32  `json:"page" form:"page" binding:"required"`
-	Limit    int32  `json:"limit" form:"limit" binding:"required"`
+	Page     int32  `json:"page" form:"page"`
+	Limit    int32  `json:"limit" form:"limit"`
 }
 
 func (server *Server) GetOrderedUsers(ctx *gin.Context) {
