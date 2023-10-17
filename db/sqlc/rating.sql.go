08c52a3c665dab0bf5d790332b2c1a48a8ac393a
isaya1910 <isaya1910@gmail.com>
2023-10-18 02:37:59 +0600

    rated users

--- db/sqlc/rating.sql.go
+++ db/sqlc/rating.sql.go
@@ -30,7 +30,7 @@ FROM user_avatar_view u
          JOIN user_roles ur ON u.id = ur.user_id AND ur.role_id = $7
 WHERE (r.brand_id = $1 OR r.brand_id IS NULL)
   AND b.id = $6
-ORDER BY r.ratio DESC
+ORDER BY r.ratio
 OFFSET $4 LIMIT $5
 `
 
@@ -113,7 +113,7 @@ FROM user_avatar_view u
      user_brand_ratio r ON u.id = r.user_id AND r.from_date = $2 AND r.to_date = $3
          JOIN user_roles ur ON u.id = ur.user_id AND ur.role_id = $6
 WHERE (r.brand_id = $1 OR r.brand_id IS NULL)
-ORDER BY r.ratio DESC
+ORDER BY r.ratio
 OFFSET $4 LIMIT $5
 `
 
