9ad7ecd1c745f73f1ecdfd1a92de6c2ead83e372
lzycodr <muhitdinov.a3@wb.ru>
2025-08-17 23:33:12 +0500

    notifications

--- pkg/notify/firebase_test.go
+++ pkg/notify/firebase_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestSendPushNotification_Integration(t *testing.T) {
 	credFile := "../../config/firebase.json"
-	token := "cmQnf7Nq3UU8j1ogwsfQjn:APA91bFHM6XloH9OGeBmlPpxYXjF2db7Kwm2rCgoaLEkQri2CwY1BWHe1P3KgLW-uW8OZDswodF2HnsCc57PwDW46xoK3yyPMBIrl92YU4RX9XufraosQgk"
+	token := ""
 
 	client, err := notify.NewFirebaseClient(credFile)
 	require.NoError(t, err)
